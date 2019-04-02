f48e9e3288537b17a627f201452058b5c280e93e
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-02 01:47:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  def initialize
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  def buy_cat(name)
+    new_cat_variable = Cat.new(name)
+    @pets[:cats] << new_cat_variable
+  end
+  def new_dog(name)
+    new_dog_variable = Dog.new(name)
+    @pets[:dogs] << new_dog_variable
+  end
+  def new_fish(name)
+    new_fish_variable = Fish.new(name)
+    @pets[:fish] << new_fish_variable
+  end
 end
